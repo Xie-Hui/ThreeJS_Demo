6117b7de7be9d6d51c7ce8f704d2016aaaca2b7e
xie-hui <polarcracker@gmail.com>
2017-09-27 20:45:39 -0700

    add the simple swith position with tween and CSSobj

--- swap.js
+++ swap.js
@@ -1,9 +1,7 @@
-var camera, scene, renderer;
-var fixcamera;
-var controler;
-var cameraHelper;
-var activecamera;
-var vanishPt;
+var camera, scene, renderer
+var vanishPt
+var objects = []
+    targets = []
 
 var sceneWebgl, rendererWebgl;
 
@@ -12,90 +10,101 @@ window.onload = function(){
     animate();
 }
 
-function transform(position, target, duration){
+function transform(targets, duration){
+
     TWEEN.removeAll();
     //move camera to starting location
-    new TWEEN.Tween(position)
-        .to(target, duration )
-        .easing( TWEEN.Easing.Exponential.InOut )
-        .onUpdate(function(){
-            camera.position.x = position.x
-            camera.position.y = position.y
-            camera.position.z = position.z
-        })
-        .start();
+    for ( i = 0; i < objects.length; i++ ) {
+
+        var object = objects[i]
+        var target = targets[i]
+
+        new TWEEN.Tween( object.position )
+            .to({ x: target.position.x, y: target.position.y, z: target.position.z }, duration )
+            .easing( TWEEN.Easing.Exponential.InOut )
+            .onUpdate( function(){
+
+                //camera.position.x = objects[0].position.x
+                //camera.position.y = objects[0].position.y
+
+            } )
+            .start();
+
+    }
+
+    new TWEEN.Tween( this )
+            .to( {}, duration * 2 )
+            .onUpdate( render )
+            .start();
+
 }
 
 function createCvElements(){
     //create the css obj for container 1
-    c1 = document.getElementById("container-1")
-    var object = new THREE.CSS3DObject(c1);
-
-    object.position.x = 0;
-    object.position.y = 0;
-    object.position.z = -200;
-    scene.add( object );
-
-    //create the css obj for container 1
-    c2 = document.getElementById("container-2")
-    console.log(c2);
-    var object = new THREE.CSS3DObject(c2);
-    object.position.x = 0;
-    object.position.y = 0;
-    object.position.z = -200;
-    scene.add( object );
-
-    position = {
-        x: 0,
-        y: 0,
-        z: 1000
-    }
-    target = {
-        x: -200,
-        y: 0,
-        z: 1000
-    }
-
-    c1.addEventListener("click", function(){
-        console.log("Click-A");
-        transform(position, target, 1000);
-    }, false)
-
-    position = {
-        x: 0,
-        y: 0,
-        z: 1000
-    }
-    target = {
-        x: 200,
-        y: 0,
-        z: 1000
-    }
-
-    c2.addEventListener("click", function(){
-        console.log("Click-B");
-        transform(position, target, 1000);
-    }, false)
 
 }
 
 function setup(){
 
-    camera = new THREE.PerspectiveCamera(38, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
-    //camera.fov = 10;
-    scene = new THREE.Scene();
-    vanishPt = new THREE.Vector3(0,0,1000);
-    camera.lookAt(vanishPt);
-    renderer = new THREE.CSS3DRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild(renderer.domElement);
-    //document.getElementById( 'container' ).appendChild( renderer.domElement );
+    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 1500;
+	scene = new THREE.Scene();
 
-    scene.add( camera );
+    //vanishPt = new THREE.Vector3(0,0,-100);
+    //camera.lookAt(vanishPt);
+    //create the css obj for container 1
+    c1 = document.createElement("div")
+    c1.className = "element"
+    c1.textContent = "AAAA"
+    var object = new THREE.CSS3DObject(c1);
+    console.log(c1);
+
+    object.position.x = -100;
+    object.position.y = 0;
+    object.position.z = 1000;
+
+    objects.push(object)
+    scene.add( object )
+
+    //create the css obj for container 1
+    c2 = document.createElement("div")
+    c2.className = "element"
+    c2.textContent = "BBB"
+    console.log(c2);
+    var object = new THREE.CSS3DObject(c2);
+    object.position.x = 100;
+    object.position.y = 0;
+    object.position.z = 1000;
+    scene.add( object );
+    objects.push(object);
+    targets[0] = objects[1]
+    targets[1] = objects[0]
+    console.log(objects);
+    console.log(targets);
+    console.log(objects[0].position, objects[1].position);
+    position = (0, 0, 100)
+    target = (-100, 0, 100)
+
+    c1.addEventListener("click", function(){
+        console.log("Click-A");
+        transform(targets, 1000);
+
+    }, false)
+
+    position = (0, 0, 100)
+    target = (100, 0, 100)
+
+    c2.addEventListener("click", function(){
+        console.log("Click-B");
+        transform(targets, 1000);
+    }, false)
+
+    renderer = new THREE.CSS3DRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.domElement.style.position = 'absolute';
+	document.getElementById( 'container' ).appendChild( renderer.domElement );
 
     //createParticles();
-    createCvElements();
 }
 
 
